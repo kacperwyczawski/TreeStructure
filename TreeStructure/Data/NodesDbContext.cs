2886ba3578250366ee994ba46641da414802da37
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-10-04 13:28:44 +0200

    Add NodesDbContext and remove WeatherForecast from Program.cs and Data/ directory

---
+++ TreeStructure/Data/NodesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TreeStructure.Models;
+
+namespace TreeStructure.Data;
+
+public class NodesDbContext : DbContext
+{
+    public NodesDbContext(DbContextOptions<NodesDbContext> options)
+        : base(options)
+    {
+    }
+    
+    public DbSet<Node> Nodes => Set<Node>();
+}
--- TreeStructure/Program.cs
+++ TreeStructure/Program.cs
@@ -4,18 +4,16 @@ using TreeStructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddSqlite<NodesDbContext>("Data Source=Nodes.db");
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
