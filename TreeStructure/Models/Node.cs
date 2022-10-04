5bfb264829ab1993adc90865c876a3063a1762af
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-10-04 12:20:31 +0200

    Add node model

---
+++ TreeStructure/Models/Node.cs
@@ -0,0 +1,9 @@
+﻿namespace TreeStructure.Models;
+
+public class Node
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int? ParentId { get; set; }
+    public List<Node> Childrens { get; set; }
+}
