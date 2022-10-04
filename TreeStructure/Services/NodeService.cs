a624e0740ce96a216727dbe2bb4e4aa1cc359f52
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-10-04 13:44:14 +0200

    Add NodeService

---
+++ TreeStructure/Services/NodeService.cs
@@ -0,0 +1,20 @@
+﻿using TreeStructure.Data;
+using TreeStructure.Models;
+
+namespace TreeStructure.Services;
+
+public class NodeService
+{
+    private readonly NodesDbContext _context;
+    
+    public NodeService(NodesDbContext context)
+    {
+        _context = context;
+    }
+    
+    public void AddNode(Node node)
+    {
+        _context.Nodes.Add(node);
+        _context.SaveChanges();
+    }
+}
