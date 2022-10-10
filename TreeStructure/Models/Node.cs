546e80bcde36c1505d26031d492667231a939527
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-10-10 10:01:50 +0200

    Suppress nullable warning with `null!` in Node model

--- TreeStructure/Models/Node.cs
+++ TreeStructure/Models/Node.cs
@@ -4,8 +4,8 @@ public class Node
 {
     public int Id { get; set; }
 
-    public string Name { get; set; }
-    
+    public string Name { get; set; } = null!;
+
     public int? ParentId { get; set; }
 
     public override string ToString() => $"{Name} #{Id} with parent #{ParentId}";
