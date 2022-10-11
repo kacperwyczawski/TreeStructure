cc214f28b4b6357f53a54886d1eb3fad3972dd00
Kacper Wyczawski <k.wyczawski@praktyki.ideo.pl>
2022-10-11 08:31:39 +0200

    Add missing files

---
+++ TreeStructure/Migrations/20221010115836_AddDisplayIdex.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TreeStructure.Migrations
+{
+    public partial class AddDisplayIdex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DisplayIndex",
+                table: "Nodes",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayIndex",
+                table: "Nodes");
+        }
+    }
+}
